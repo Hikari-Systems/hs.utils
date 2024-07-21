281c25f5f38bf57c643387bb20a1990624f86bab
Rick Knowles <rickknowles.i@gmail.com>
2024-07-21 17:06:13 +0100

    added secret wrapper

--- lib/config.ts
+++ lib/config.ts
@@ -1,10 +1,21 @@
 import nconf from 'nconf';
+import { readFileSync } from 'fs';
+
+const SECRET_PREFIX = '[SECRET]:';
 
 const config = nconf
   .argv()
   .env('__')
-  .file('secrets', '/run/secrets/config.json')
   .file('environment', '/sandbox/config.json')
   .file('defaults', 'config.json');
 
-export default config;
+const get = (key: string) => {
+  const val = config.get(key);
+  if (val && `${val}`.startsWith(SECRET_PREFIX)) {
+    const filename = `${val}`.substring(SECRET_PREFIX.length);
+    return readFileSync(filename, { encoding: 'utf-8', flag: 'r' });
+  }
+  return val;
+};
+
+export default { get }; // export a wrapped nconf.get()
